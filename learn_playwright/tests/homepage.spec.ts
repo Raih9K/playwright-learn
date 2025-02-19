312655f0945087eab3b6dd50cebd186c2fd33b07
Raihan Khan <71407079+Raih9K@users.noreply.github.com>
2025-02-20 00:03:44 +0600

    task basics

--- learn_playwright/tests/homepage.spec.ts
+++ learn_playwright/tests/homepage.spec.ts
@@ -1,20 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('home page test', async ({ page, request }) => {
-  await page.goto('https://lms.boostonamazon.com/');
-  await page.getByText('Self Onboarding').first().click();
-  await page.getByText('Integration', { exact: true }).first().click();
-  await page.getByText('Inventory', { exact: true }).first().click();
-  await page.getByText('Listing', { exact: true }).click();
-  await page.locator('div').filter({ hasText: /^Syncing$/ }).first().click();
-  await page.locator('div').filter({ hasText: /^Order$/ }).first().click();
-  await page.getByText('Warehouse', { exact: true }).first().click();
-  await page.getByRole('link', { name: 'Contact Support' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).first().click();
-  await page.getByRole('textbox', { name: 'First Name' }).nth(1).click();
-  await page.getByRole('textbox', { name: 'Email', exact: true }).click();
-  await page.getByRole('textbox', { name: 'Phone Number' }).click();
-  await page.getByRole('textbox', { name: 'Company Name' }).click();
-  await page.locator('div').filter({ hasText: /^Order Volume0 - 100100 - 500500 - 1000$/ }).getByRole('combobox').selectOption('high');
-  await page.getByRole('combobox').nth(1).selectOption('low');
+  await page.goto('http://website.dev.myqbits.com/');
+  await expect(page.locator('#sigmax2')).toContainText('For Power Usages');
+  await expect(page.locator('#sigmax2')).toContainText('Buy Now');
+  await expect(page.locator('#root')).toContainText('Register Your Device');
+});
+
+test('home page test2', async ({ page, request }) => {
+  await page.goto('http://website.dev.myqbits.com/');
+  await expect(page.locator('#sigmax2')).toContainText('For Power Usages');
+  await expect(page.locator('#sigmax2')).toContainText('Buy Now');
+  await expect(page.locator('#root')).toContainText('Register Your Device');
 });
