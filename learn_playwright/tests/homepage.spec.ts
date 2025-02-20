8aeb83aa4a4659e9ced12921ab51cbda649e5faf
Raihan Khan <71407079+Raih9K@users.noreply.github.com>
2025-02-20 21:55:18 +0600

    Update homepage.spec.ts

--- learn_playwright/tests/homepage.spec.ts
+++ learn_playwright/tests/homepage.spec.ts
@@ -7,6 +7,7 @@ test('home page test', async ({ page, request }) => {
   await expect(page.locator('#root')).toContainText('Register Your Device');
 });
 
+
 test('home page test2', async ({ page, request }) => {
   await page.goto('http://website.dev.myqbits.com/');
   await expect(page.locator('#sigmax2')).toContainText('For Power Usages');
